ca4bd99d678e3555b61a7f7aea64acafa9f34872
Harish-Dev-777 <harishmkdev@gmail.com>
2026-02-03 12:34:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./components/nav";
+
+const App = () => {
+  return (
+    <div>
+      <Nav />
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/nav.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <h2>Logo</h2>
+      <nav>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+        <li>Home</li>
+      </nav>
+      <div>
+        <button>Get started</button>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
